1f2983011a5c038892ddcbf1547203294ecbe48a
AMixailV <Mixer-X@mail.ru>
2024-06-15 09:43:56 +0300

    Initial commit

---
+++ app/src/main/java/ru/mixail_akulov/a1_frl_linl_grl/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.mixail_akulov.a1_frl_linl_grl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
